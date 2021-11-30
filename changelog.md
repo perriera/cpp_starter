8b80196e1257f9e41fd3f9d303d10beefd35580c
Perry Anderson <perry.anderson@gmail.com>
2021-11-29 16:49:52 -0800

    Added v.0.10.0

--- changelog.md
+++ changelog.md
@@ -4,8 +4,8 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), 
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).</br>
 > To add a tag to the git repository do:
-> > git tag -a v0.9.0 -m "Added MAKE_CPP_STARTER_LIBRARY_ONLY"</br>
-> > git push origin v0.9.0
+> > git tag -a v0.10.0 -m "Added LICENSE.md"</br>
+> > git push origin v0.10.0
 > 
 
 IMPORTANT: 
@@ -28,6 +28,11 @@ Correct URL for github, (for source compare)</br>
 Adding mock test cases </br>
 Adding unit test cases </br>
 Updated README.md accordinging</br>
+
+#### 0.10.0 (2021-11-29)
+### Changed 
+- Using extras 5.4.0
+- Added LICENSE.md 
 
 #### 0.9.0 (2021-11-15)
 ### Changed 
@@ -149,7 +154,8 @@ Updated README.md accordinging</br>
 ### Removed
 - Conan package manager deprecated
 
-[Unreleased]: https://github.com/perriera/cpp_starter/compare/v0.9.0...HEAD
+[Unreleased]: https://github.com/perriera/cpp_starter/compare/v0.10.0...HEAD
+[0.10.0]: https://github.com/perriera/cpp_starter/compare/v0.9.0...v0.10.0
 [0.9.0]: https://github.com/perriera/cpp_starter/compare/v0.8.4...v0.9.0
 [0.8.0]: https://github.com/perriera/cpp_starter/compare/v0.7.0...v0.8.4
 [0.7.0]: https://github.com/perriera/cpp_starter/compare/v0.6.6...v0.7.0
--- instances/main.cpp
+++ instances/main.cpp
@@ -1,11 +1,47 @@
+/**
+ * @brief the "MIT/X Consortium License", (adapted for EXPARX.COM)
+ *
+ * Copyright (C) November 22, 2021 EXPARX INCORPORATED
+ *
+ * Permission is hereby  granted,  free of charge,  to  any  person
+ * obtaining a copy of this software and  associated  documentation
+ * files   (the  "Software"),  to deal  in   the  Software  without
+ * restriction, including  without  limitation the rights  to  use,
+ * copy,  modify, merge,  publish,  distribute,  sublicense, and/or
+ * sell copies of the  Software, and to permit persons  to whom the
+ * Software  is  furnished to  do  so,  subject  to  the  following
+ * conditions:
+ *
+ * The above copyright notice and  this permission  notice shall be
+ * included in all copies or  substantial portions of the Software.
+ *
+ * THE SOFTWARE IS  PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESSED  OR   IMPLIED,  INCLUDING   BUT  NOT  LIMITED  TO  THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A  PARTICULAR PURPOSE
+ * AND NON-INFRINGEMENT.  IN  NO  EVENT  SHALL EXPARX  INCORPORATED
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER  IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM, OUT  OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Except  as  contained  in this  notice, the  name of  the EXPARX
+ * INCORPORATED shall not  be used in  advertising or  otherwise to
+ * promote the sale, use or other dealings in this Software without
+ * prior written authorization from EXPARX INCORPORATED.
+ *
+ * exparx.com and www.exparx.com  are domain names  registered with
+ * EXPARX INCORPORATED, (other GPL-themed licenses are included).
+ *
+ */
+
 #include <iostream>
 #include <filesystem>
 
-// #
-// # CMakeLists.txt uses a GLOB_RECURSE macro which automatically includes ALL headers 
-// # and sources under include/ and src/. So, to keep two or more main() declarations 
-// # seperate, we place any sources that have a main() in the instances/ folder
-// #
+ // #
+ // # CMakeLists.txt uses a GLOB_RECURSE macro which automatically includes ALL headers 
+ // # and sources under include/ and src/. So, to keep two or more main() declarations 
+ // # seperate, we place any sources that have a main() in the instances/ folder
+ // #
 
 int main(int, const char**)
 {
