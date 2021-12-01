066dd8ba955453d15e4d0571e732d8d709ba8554
Perry Anderson <perry.anderson@gmail.com>
2021-11-30 23:12:36 -0800

    Added v0.11.0

--- test/chessgame/mock_ChessGame.cpp
+++ test/chessgame/mock_ChessGame.cpp
@@ -12,30 +12,12 @@
  * Software  is  furnished to  do  so,  subject  to  the  following
  * conditions:
  *
- * The above copyright notice and  this permission  notice shall be
- * included in all copies or  substantial portions of the Software.
- *
- * THE SOFTWARE IS  PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESSED  OR   IMPLIED,  INCLUDING   BUT  NOT  LIMITED  TO  THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A  PARTICULAR PURPOSE
- * AND NON-INFRINGEMENT.  IN  NO  EVENT  SHALL EXPARX  INCORPORATED
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER  IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM, OUT  OF
- * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  OTHER DEALINGS
- * IN THE SOFTWARE.
- *
- * Except  as  contained  in this  notice, the  name of  the EXPARX
- * INCORPORATED shall not  be used in  advertising or  otherwise to
- * promote the sale, use or other dealings in this Software without
- * prior written authorization from EXPARX INCORPORATED.
- *
- * exparx.com and www.exparx.com  are domain names  registered with
- * EXPARX INCORPORATED, (other GPL-themed licenses are included).
+ * (See LICENSE.md for complete details)
  *
  */
 
 #include <iostream>
-#include "cpp_starter/game/ChessGame.hpp"
+#include <cpp_starter/game/ChessGame.hpp>
 
 #include "../vendor/catch.hpp"
 #include "../vendor/fakeit.hpp"
@@ -45,10 +27,10 @@ using namespace fakeit;
 
 SCENARIO("Mock ChessGameInterface: toOctal", "[CHES-9]") {
 
-    Mock<ChessGameInterface> mock;
+    Mock<cpp::ChessGameInterface> mock;
     When(Method(mock, moves)).Return();
 
-    ChessGameInterface& i = mock.get();
+    cpp::ChessGameInterface& i = mock.get();
     i.moves();
     Verify(Method(mock, moves));
 }
